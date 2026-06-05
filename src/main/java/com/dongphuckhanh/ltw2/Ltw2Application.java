5c8a05eb32fe1ba9bad2a987371f723a5c4b96ee
DongPhucKhanh <khanh0387093523@gmail.com>
2026-06-05 08:27:53 +0700

    update

---
+++ src/main/java/com/dongphuckhanh/ltw2/Ltw2Application.java
@@ -0,0 +1,13 @@
+package com.dongphuckhanh.ltw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ltw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ltw2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dongphuckhanh/ltw2/Ltw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongphuckhanh.ltw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ltw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
